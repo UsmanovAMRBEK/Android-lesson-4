400618b69da852513cee75c193f83abc90a5a673
UsmanovAMRBEK <princariens@gmail.com>
2023-07-31 17:09:55 +0500

    one :)

---
+++ app/src/main/java/com/example/androidlesson004/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidlesson004;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen7days);
+    }
+}
